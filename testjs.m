ef165ee30ee7c49a46741b838d092830a7c930a3
RocketWrench <67157451+RocketWrench@users.noreply.github.com>
2023-01-17 15:22:07 -0500

    Add files via upload

---
+++ testjs.m
@@ -0,0 +1,16 @@
+function testjs
+
+    htmlfile = 'gage.html';
+    htmlcode = readlines(htmlfile,'EmptyLineRule','skip','WhitespaceRule','trim');
+    htmlcode = char(join(htmlcode));
+
+    jscode = 'var canvasLeft = document.getElementById("right");ctx = canvasLeft.getContext("2d");ctx.font = "40px Arial";ctx.strokeText("Right Side",10,60)';
+
+    b = web.Browser([],figure('Position',[447 702 700 400]));
+    drawnow()
+    b.loadString(htmlcode)
+    drawnow()
+    pause(0.5)
+    b.executeJavaScript(jscode,b.URL,0)
+    
+end    
